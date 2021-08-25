b33c6c52d70ec4864a644ac363c790a15ef6de2c
example <email@example.com>
2021-08-25 18:32:28 +0300

    ilk commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IF_ELSE_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time>=6 && time<=12)
+            {
+                Console.WriteLine("Günaydın!");
+            }
+            else if (time<18 && time>12)
+            {
+                System.Console.WriteLine("İyi Günler");
+            }
+            else if (time>=18)
+            {
+                System.Console.WriteLine("İyi Akşamlar!");
+            }
+            
+            string sonuc = time>=6 && time<12 ? "Günaydın" : time>12 && time<18 ? "İyi Günler" : "İyi Akşamlar";
+            System.Console.WriteLine(sonuc);
+
+            Console.ReadLine();
+
+        }
+    }
+}
